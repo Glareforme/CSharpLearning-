337da849ce19fee02033769201fcc56fc351fb91
NIX\nhyiebenh <stiven.nhyiebenh@nixsolutions.com>
2022-12-20 23:36:22 +0200

    bd update

---
+++ TestsForTests/DBFirstApproach/GetDataInDB/GetDataFromCustomerTable.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+namespace DBFirstApproach.GetDataInDB
+{
+    public class GetDataFromCustomerTable
+    {
+        public static List<string> GetContactNameFromCustomerTable()
+        {
+            using (NorthwindEntities db = new NorthwindEntities())
+            {
+                var customers = db.Customers.ToList();
+                var listOfContactName = new List<string>();
+                foreach (var item in customers)
+                {
+                    listOfContactName.Add(item.ContactName);
+                }
+                return listOfContactName;
+            }
+        }
+
+        public static List<string> GetCompanyNameFromCustomerTable()
+        {
+            using (NorthwindEntities db = new NorthwindEntities())
+            {
+                var customers = db.Customers.ToList();
+                var listCompanyNames = new List<string>();
+                foreach (var item in customers)
+                {
+                    listCompanyNames.Add(item.CompanyName);
+                }
+                return listCompanyNames;
+            }
+        }
+    }
+}
---
+++ TestsForTests/DBFirstApproach/TestController/DBFTestsGetData.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using DBFirstApproach.GetDataInDB;
+
+namespace DBFirstApproach
+{
+    internal class DBFTestsGetData
+    {
+        [Test]
+        public void CheckIfCanTakeContactNamesFromCustomersTable()
+        {
+            var actualResult = GetDataFromCustomerTable.GetContactNameFromCustomerTable();
+            Assert.IsNotNull(actualResult);
+        }
+
+        [Test]
+        public void CheckIfDataBaseReturnedCorrectCompanyNamesFromCustomersTable()
+        {
+
+        }
+
+        [Test]
+        public void CheckIfContactNameAndCompanyNameCorresponded()
+        {
+
+        }
+    }
+}
